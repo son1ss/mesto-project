b66dace8d5ea924e49443292b693580be0ea48dc
son1s <fomin.ea2003@gmail.com>
2023-05-19 17:21:04 +0300

    fix: event listeners

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,23 +1,23 @@
 import { imagePopup, imgLink, imgCaption } from './utils'
 
-const handleEsc = (popup) => {
-  return e => {if (e.key === 'Escape') {closePopup(popup)}}
+const handleEsc = e => {
+  if (e.key === 'Escape') {closePopup(document.querySelector('.popup_active'))}
 }
 
-const handleOverlayClose = (popup) => {
-  return e => {if(e.target.className.includes('popup_active')) {closePopup(popup)}}
+const handleOverlayClose = e => {
+  if(e.target.className.includes('popup_active')) {closePopup(e.target)}
 }
 
 export const openPopup = popup => {
   popup.classList.add('popup_active')
-  document.addEventListener('keydown', handleEsc(popup))
-  popup.addEventListener('click', handleOverlayClose(popup))
+  document.addEventListener('keydown', handleEsc)
+  popup.addEventListener('click', handleOverlayClose)
 }
 
 export const closePopup = popup => {
   popup.classList.remove('popup_active')
-  document.removeEventListener('keydown', handleEsc(popup))
-  popup.removeEventListener('click', handleOverlayClose(popup))
+  document.removeEventListener('keydown', handleEsc)
+  popup.removeEventListener('click', handleOverlayClose)
 }
 export const imagePopupHandler = (cardObject) => {
   openPopup(imagePopup);
